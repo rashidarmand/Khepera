9e4d3a5ae91c2e674878e49ffc83f36883941576
Rashid Armand <rashidharmand@gmail.com>
2021-02-15 23:59:23 -0500

    setup styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import '../styles/globals.scss';
+
+import { ChakraProvider } from '@chakra-ui/react';
+import { css, Global } from '@emotion/react';
+import theme from '@lib/theme';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;

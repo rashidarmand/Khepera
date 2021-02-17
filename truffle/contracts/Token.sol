fa1607aa33b1e98731fdf105f4db19affa5d3c6d
Rashid Armand <rashidharmand@gmail.com>
2021-02-17 18:02:24 -0500

    implemented more of the ERC-20 standards on the Khepera token

--- truffle/contracts/Token.sol
+++ truffle/contracts/Token.sol
@@ -1,6 +1,37 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.6.0 <0.8.0;
+
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract Token {
-	string public name = "Khepera";
+	using SafeMath for uint;
+
+	string public constant name = "Khepera";
+	string public constant symbol = "KHEP";
+	uint256 public constant decimals = 18;
+	uint256 public totalSupply;
+	// Track Balances	
+	mapping(address => uint256) public balanceOf;
+
+	// Events
+	event Transfer(address indexed from, address indexed to, uint256 value);
+
+	constructor() public {
+		totalSupply = 1000000 * (10 ** decimals);
+		balanceOf[msg.sender] = totalSupply;
+	}
+
+	// Send Tokens
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		require(_to != address(0));
+		require(balanceOf[msg.sender] >= _value);
+
+		balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+		balanceOf[_to] = balanceOf[_to].add(_value);
+
+		emit Transfer(msg.sender, _to, _value);
+
+		return true;
+	}
+
 }

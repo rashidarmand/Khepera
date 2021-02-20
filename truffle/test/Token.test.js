588e9510a99ae54d9775054b8363145ce975201c
Rashid Armand <rashidharmand@gmail.com>
2021-02-20 02:46:21 -0500

    refactor to limit confusion by calling token 'token' in test file

--- truffle/test/Token.test.js
+++ truffle/test/Token.test.js
@@ -6,39 +6,39 @@ const Token = artifacts.require('Token');
 require('chai').use(require('chai-as-promised')).should();
 
 contract('Token', ([deployer, receiver, exchange]) => {
-  let khepera;
+  let token;
   const name = 'Khepera';
   const symbol = 'KHEP';
   const decimals = '18';
   const totalSupply = tokens(1000000);
 
   beforeEach(async () => {
-    khepera = await Token.new();
+    token = await Token.new();
   });
 
   describe('deployment', () => {
     it('tracks the name', async () => {
-      const result = await khepera.name();
+      const result = await token.name();
       result.should.equal(name);
     });
 
     it('tracks the symbol', async () => {
-      const result = await khepera.symbol();
+      const result = await token.symbol();
       result.should.equal(symbol);
     });
 
     it('tracks the decimals', async () => {
-      const result = await khepera.decimals();
+      const result = await token.decimals();
       result.toString().should.equal(decimals);
     });
 
     it('tracks the total supply', async () => {
-      const result = await khepera.totalSupply();
+      const result = await token.totalSupply();
       result.toString().should.equal(totalSupply);
     });
 
     it('assigns the total supply of tokens to the deployer', async () => {
-      const result = await khepera.balanceOf(deployer);
+      const result = await token.balanceOf(deployer);
       result.toString().should.equal(totalSupply);
     });
   });
@@ -49,14 +49,14 @@ contract('Token', ([deployer, receiver, exchange]) => {
     describe('success', async () => {
       beforeEach(async () => {
         amount = tokens(100);
-        result = await khepera.transfer(receiver, amount, { from: deployer });
+        result = await token.transfer(receiver, amount, { from: deployer });
       });
 
       it('transfers token balances', async () => {
         let balanceOf;
-        balanceOf = await khepera.balanceOf(deployer);
+        balanceOf = await token.balanceOf(deployer);
         balanceOf.toString().should.equal(tokens(999900));
-        balanceOf = await khepera.balanceOf(receiver);
+        balanceOf = await token.balanceOf(receiver);
         balanceOf.toString().should.equal(tokens(100));
       });
 
@@ -74,15 +74,15 @@ contract('Token', ([deployer, receiver, exchange]) => {
       it('rejects insufficient balances', async () => {
         let invalidAmount;
         invalidAmount = tokens(100000000); // 100 million - greater than total supply
-        await khepera.transfer(receiver, invalidAmount, { from: deployer }).should.be.rejectedWith(EVM_REVERT);
+        await token.transfer(receiver, invalidAmount, { from: deployer }).should.be.rejectedWith(EVM_REVERT);
 
         // Attempt to transfer tokens when you have none
         invalidAmount = tokens(10); // recipient has no tokens
-        await khepera.transfer(deployer, invalidAmount, { from: receiver }).should.be.rejectedWith(EVM_REVERT);
+        await token.transfer(deployer, invalidAmount, { from: receiver }).should.be.rejectedWith(EVM_REVERT);
       });
 
       it('rejects invalid recipients', async () => {
-        await khepera.transfer(0x0, amount, { from: deployer }).should.be.rejected;
+        await token.transfer(0x0, amount, { from: deployer }).should.be.rejected;
       });
     });
   });
@@ -93,11 +93,11 @@ contract('Token', ([deployer, receiver, exchange]) => {
     describe('success', async () => {
       beforeEach(async () => {
         amount = tokens(100);
-        result = await khepera.approve(exchange, amount, { from: deployer });
+        result = await token.approve(exchange, amount, { from: deployer });
       });
 
       it('allocates an allowance for delegated token spending', async () => {
-        const allowance = await khepera.allowance(deployer, exchange);
+        const allowance = await token.allowance(deployer, exchange);
         allowance.toString().should.equal(amount);
       });
 
@@ -113,7 +113,7 @@ contract('Token', ([deployer, receiver, exchange]) => {
 
     describe('failure', async () => {
       it('rejects invalid spenders', async () => {
-        await khepera.approve(0x0, amount, { from: deployer }).should.be.rejected;
+        await token.approve(0x0, amount, { from: deployer }).should.be.rejected;
       });
     });
   });
@@ -123,24 +123,24 @@ contract('Token', ([deployer, receiver, exchange]) => {
 
     beforeEach(async () => {
       amount = tokens(100);
-      await khepera.approve(exchange, amount, { from: deployer });
+      await token.approve(exchange, amount, { from: deployer });
     });
 
     describe('success', async () => {
       beforeEach(async () => {
-        result = await khepera.transferFrom(deployer, receiver, amount, { from: exchange });
+        result = await token.transferFrom(deployer, receiver, amount, { from: exchange });
       });
 
       it('transfers token balances', async () => {
         let balanceOf;
-        balanceOf = await khepera.balanceOf(deployer);
+        balanceOf = await token.balanceOf(deployer);
         balanceOf.toString().should.equal(tokens(999900));
-        balanceOf = await khepera.balanceOf(receiver);
+        balanceOf = await token.balanceOf(receiver);
         balanceOf.toString().should.equal(tokens(100));
       });
 
       it('resets the allowance', async () => {
-        const allowance = await khepera.allowance(deployer, exchange);
+        const allowance = await token.allowance(deployer, exchange);
         allowance.toString().should.equal('0');
       });
 
@@ -158,12 +158,12 @@ contract('Token', ([deployer, receiver, exchange]) => {
       it('rejects insufficient amounts', async () => {
         // Attempt to transfer too many tokens
         const invalidAmount = tokens(100000000);
-        await khepera
+        await token
           .transferFrom(deployer, receiver, invalidAmount, { from: exchange })
           .should.be.rejectedWith(EVM_REVERT);
       });
       it('rejects invalid recipients', async () => {
-        await khepera.transferFrom(deployer, 0x0, amount, { from: exchange }).should.be.rejected;
+        await token.transferFrom(deployer, 0x0, amount, { from: exchange }).should.be.rejected;
       });
     });
   });

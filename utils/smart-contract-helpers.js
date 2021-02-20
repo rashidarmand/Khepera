fe5678e308eeb7670cdff72b7342e78346c9ab64
Rashid Armand <rashidharmand@gmail.com>
2021-02-20 02:46:47 -0500

    more helpers

--- utils/smart-contract-helpers.js
+++ utils/smart-contract-helpers.js
@@ -1,8 +1,13 @@
 const Web3 = require('web3');
 
-export const tokens = (numberOfTokens) => {
+export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+export const EVM_REVERT = 'VM Exception while processing transaction: revert';
+
+export const ether = (numberOfTokens) => {
   const { BN, toWei } = Web3.utils;
   return new BN(toWei(numberOfTokens.toString(), 'ether')).toString();
 };
 
-export const EVM_REVERT = 'VM Exception while processing transaction: revert';
+// Same as ether
+export const tokens = (n) => ether(n);

6b0d732bff6618635ad8f38db8f57e2821e4388f
Rashid Armand <rashidharmand@gmail.com>
2021-02-20 05:04:00 -0500

    wip: working on a function to abstract away the test creation for events

--- utils/smart-contract-helpers.js
+++ utils/smart-contract-helpers.js
@@ -11,3 +11,38 @@ export const ether = (numberOfTokens) => {
 
 // Same as ether
 export const tokens = (n) => ether(n);
+
+const createEventParamAssertion = (args) => (eventEmitterTestCriteria) => {
+  const { name, value, type, msg } = eventEmitterTestCriteria;
+  const message = msg || `${name} is correct.`;
+  if (!type || type !== 'number') {
+    args[name].should.equal(value, message);
+  } else {
+    args[name].toString().should.equal(value, message);
+  }
+};
+
+// TODO: convert to typescript to use interfaces
+/**
+ * interface EventParams = {
+ * 	name: string
+ * 	value: any
+ * 	type?: address(string) | number // Default is string
+ * 	msg?: string
+ * }
+ *
+ * @param {
+ * 		result: any
+ * 		eventName: string
+ * 		eventParams: EventParams[]
+ * } eventTestParams
+ */
+// * WIP - weird issue with result being undefined
+export const testForEvent = (eventTestParams) => {
+  const { result, eventName, eventParams } = eventTestParams;
+  console.log('result:: ', eventName);
+  const log = result.logs[0];
+  const { event, args } = log;
+  event.should.equal(eventName);
+  eventParams.forEach(createEventParamAssertion(args));
+};

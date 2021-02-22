e54723845801f7503b9f5d149bc035c99a4ff893
Rashid Armand <rashidharmand@gmail.com>
2021-02-21 19:24:09 -0500

    milestone: exchange & token contracts deployed

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,6 +1,12 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 const Token = artifacts.require('Token');
+const Exchange = artifacts.require('Exchange');
 
-module.exports = function (deployer) {
-  deployer.deploy(Token);
+module.exports = async function (deployer) {
+  const accounts = await web3.eth.getAccounts(); // web3 injected by truffle suite
+  const [feeAccount] = accounts;
+  const feePercent = 1;
+
+  await deployer.deploy(Token);
+  await deployer.deploy(Exchange, feeAccount, feePercent);
 };

653be413908ea20327852cb94485e2b448019c27
Rashid Armand <rashidharmand@gmail.com>
2021-02-26 02:26:59 -0500

    milestone: order book component using actual data from the blockchain

--- components/OrderBook.js
+++ components/OrderBook.js
@@ -1,7 +1,44 @@
-import { Box, Flex, Heading, Table, Tbody, Td, Tr } from '@chakra-ui/react';
+import { Box, Flex, Heading, Spinner, Table, Tbody, Td, Tr } from '@chakra-ui/react';
+import { orderBookLoadedSelector, orderBookSelector } from '@store/selectors';
+import { useSelector } from 'react-redux';
+
+const OrderBookTableRows = ({ orderBook }) => {
+  const { buy, sell } = orderBook;
+
+  const renderOrderRow = (order) => (
+    <Tr key={order.id}>
+      <Td>{order.tokenAmount}</Td>
+      <Td color={order.orderTypeColor}>{order.tokenPrice}</Td>
+      <Td isNumeric>{order.etherAmount}</Td>
+    </Tr>
+  );
+
+  return (
+    <>
+      {sell.map(renderOrderRow)}
+      <Tr>
+        <Td fontWeight="bold">KHEP</Td>
+        <Td fontWeight="bold">KHEP/ETH</Td>
+        <Td fontWeight="bold" textAlign="center">
+          ETH
+        </Td>
+      </Tr>
+      {buy.map(renderOrderRow)}
+    </>
+  );
+};
+
+const LoadingTableRows = () => (
+  <Tr>
+    <Td colSpan="3" pt="6" textAlign="center">
+      <Spinner size="xl" />
+    </Td>
+  </Tr>
+);
 
 const OrderBook = () => {
-  const _numberOfOrders = Array.from({ length: 9 }, (_, i) => i);
+  const orderBook = useSelector(orderBookSelector);
+  const showOrderBook = useSelector(orderBookLoadedSelector);
 
   return (
     <Flex h="100%" direction="column">
@@ -11,29 +48,7 @@ const OrderBook = () => {
 
       <Box flexGrow="1" mt="4">
         <Table size="sm" h="100%">
-          <Tbody>
-            {_numberOfOrders.map((o) => (
-              <Tr key={o}>
-                <Td>100</Td>
-                <Td color="red">2.00008</Td>
-                <Td isNumeric>0.01</Td>
-              </Tr>
-            ))}
-            <Tr>
-              <Td fontWeight="bold">KHEP</Td>
-              <Td fontWeight="bold">KHEP/ETH</Td>
-              <Td fontWeight="bold" textAlign="center">
-                ETH
-              </Td>
-            </Tr>
-            {_numberOfOrders.map((o) => (
-              <Tr key={o}>
-                <Td>100</Td>
-                <Td color="green">2.00008</Td>
-                <Td isNumeric>0.01</Td>
-              </Tr>
-            ))}
-          </Tbody>
+          <Tbody>{showOrderBook ? <OrderBookTableRows orderBook={orderBook} /> : <LoadingTableRows />}</Tbody>
         </Table>
       </Box>
     </Flex>

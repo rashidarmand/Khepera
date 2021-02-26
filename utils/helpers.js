1adc0d5c527582be22016985bd314102a68c89d1
Rashid Armand <rashidharmand@gmail.com>
2021-02-25 21:16:31 -0500

    update: helpers for frontend

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
 export const colorLog = (message, ...optionalParams) => {
   if (!optionalParams) {
-    console.log(`%c ${message}`, 'background: #222; color: #bada55');
+    console.log(`%c ${message}`, 'background: #222; color: #F6E05E');
   } else {
-    console.log(`%c ${message}`, 'background: #222; color: #bada55', ...optionalParams);
+    console.log(`%c ${message}`, 'background: #222; color: #F6E05E', ...optionalParams);
   }
 };
 
@@ -11,3 +11,12 @@ export const wait = (seconds) => {
   const milliseconds = seconds * 1000;
   return new Promise((resolve) => setTimeout(resolve, milliseconds));
 };
+
+export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+export const DECIMALS = 10 ** 18;
+
+// Different from smart-contract-helper ether function because it trims traling zeroes for display in UI
+export const ether = (wei) => wei / DECIMALS;
+
+export const tokens = ether;
